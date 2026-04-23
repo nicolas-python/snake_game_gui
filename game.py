0fbd48c70d1551b40eeb6a2f0002f88ed11f6451
Nicolas <nicolasmader001@gmail.com>
2026-04-23 13:32:20 +0200

    add snake head on canvas

--- game.py
+++ game.py
@@ -10,4 +10,11 @@ def snake():
     canvas = tk.Canvas(root, width=400, height=400, bg="black")
     canvas.pack()
 
+    #schlange model
+    canvas.create_rectangle(190, 190, 210, 210, fill="green")
+
+    #map
+
+    #steuerung (pfeiltasten)
+
     root.mainloop()
